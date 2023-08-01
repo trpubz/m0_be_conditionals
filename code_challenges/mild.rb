294a99fae1c14721cf8f9840c8d71a1106b8cad3
Taylor Pubins <tpubz@icloud.com>
2023-08-01 15:15:19 -0700

    Complete mild challenge

---
+++ code_challenges/mild.rb
@@ -0,0 +1,10 @@
+# Reqs
+# Write a Ruby program that defines a variable that stores an Integer. The program should print out the String "even" if the Integer is even, and the String "odd" if the Integer is odd.
+
+my_mild_integer = 3
+
+if my_mild_integer % 2 == 0
+  puts "even"
+else
+  puts "odd"
+end
